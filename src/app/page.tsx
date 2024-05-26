52765159103e31659a4268ec894399d9eefcaafb
SeaUrc <nicksong03@gmail.com>
2024-05-25 22:32:23 -0400

    table changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,17 @@ export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
   headers();
-  const images = await db.query.images.findMany();
+  const images = await db.query.images.findMany({
+    orderBy: (model, {desc}) => desc(model.id),
+  });
 
   return (
     <main className="">
-      <div className="flex"> 
+      <div className="flex flex-wrap gap-4">
         {images.map((image) => (
-          <div key={image.id} className="w-48 p-4">
+          <div key={image.id} className="flex flex-col w-64">
             <img src={image.url} />
+            <div>{image.name}</div>
           </div>
         ))}
       </div>
